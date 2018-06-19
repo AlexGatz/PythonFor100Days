338c6aabca7d96010ce9af9588cf4a3bbc651963
Alex J. Gatz <18584316+TheNeoCoder@users.noreply.github.com>
2018-06-19 14:41:01 -0400

    Create KeyAndValueSort.py
    This is a little code I was using to play with sorting values and keys in a dict.

---
+++ RandomScripts/KeyAndValueSort.py
@@ -0,0 +1,15 @@
+"""
+Created By: Alex J. Gatz
+Date: 06/05/2018
+Some random test code playing with sorting a dictionary and outputing the sorted values inna list.
+"""
+
+nameAge_dict = {'Alex':27, 'Taunee':26, 'Andrew':25, 'James':50, 'Marla':49}
+
+# This is really key sort and value sort 
+alphaSort = sorted(nameAge_dict, key=str.lower)
+ageSort = sorted(nameAge_dict.values())
+
+print(nameAge_dict)
+print(alphaSort)
+print(ageSort)
