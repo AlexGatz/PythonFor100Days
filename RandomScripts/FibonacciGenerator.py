15c4813d662ceacc39f58e84f3204fdfc2a02338
Alex J. Gatz <18584316+TheNeoCoder@users.noreply.github.com>
2018-06-19 14:57:11 -0400

    Create FibonacciGenerator.py

---
+++ RandomScripts/FibonacciGenerator.py
@@ -0,0 +1,26 @@
+"""
+Created By: Alex J. Gatz
+Date: 06/15/2018
+
+This is some code playijg with the usage of a python 
+"Generator" which is really very cool. Another use case
+I want to play with is properly ordering installation of
+packages to ensure that if there are dependencies that they are installed in the proper order. 
+"""
+
+# Fibonacci generator
+def fibonacci(n): 
+    a, b, counter = 0, 1, 0 
+    while True: 
+        if (counter > n): 
+            return 
+        yield a 
+        a, b = b, a + b 
+        counter += 1 
+
+# How many values donyou want?
+f = fibonacci(30) 
+
+# Iterate multiple calls of the fibonacci generator
+for x in f: 
+    print(x, " ", end="")
