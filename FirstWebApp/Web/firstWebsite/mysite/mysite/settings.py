cfd014ba512007c03e5e0174ebba6ac48d8fa7ce
Alex J. Gatz <18584316+TheNeoCoder@users.noreply.github.com>
2018-06-26 23:34:29 -0400

    Update settings.py

--- FirstWebApp/Web/firstWebsite/mysite/mysite/settings.py
+++ FirstWebApp/Web/firstWebsite/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k2$b#!%q&zoij69d45dhukuv99qf82fcp3g9-$)-%n+0dx7z$&'
+SECRET_KEY = 'A Secret Key Goes Here...'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
