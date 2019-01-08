ae2afb9162d6b2d247fd2082b66d2b2870b91d54
Alex J. Gatz <the.neocoder@gmail.com>
2019-01-08 08:14:51 -0500

    Create imageResizer.py

---
+++ RandomScripts/imageResizer.py
@@ -0,0 +1,25 @@
+import sys 
+import cv2
+import os
+
+width = int(1920/2)
+height = int(1080/2)
+dim = (width, height)
+
+def resizeImages(folder):
+    for filename in os.listdir(folder):
+        print(filename)
+        img = cv2.imread(os.path.join(folder,filename))
+        print('Original Dimensions : ',img.shape)
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        print('Resized Dimensions : ',resized.shape)
+        resizedFileName = f'{filename}_resized.jpg'
+        cv2.imwrite(resizedFileName, resized)
+        print(resizedFileName)
+
+def main():
+    path = "/home/neo/Code/PythonStuff/PythonFor100Days/testImages"
+    resizeImages(path)
+
+if __name__ == "__main__":
+    main()
